a6b06bb9f095c8c5b41e1ff4084353691e19c184
SaefullohAziz <16171234@smkifsmd.sch.id>
2019-09-09 09:40:31 +0700

    act

--- app/Http/Controllers/Admin/ActivityController.php
+++ app/Http/Controllers/Admin/ActivityController.php
@@ -29,6 +29,7 @@ class ActivityController extends Controller
 
     public function __construct()
     {
+       
         $this->middleware('auth:admin');
         $this->createdMessage = __('Data successfully created.');
         $this->updatedMessage = __('Data successfully updated.');
