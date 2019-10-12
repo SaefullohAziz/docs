d21865ba9991a4d704934029d97e2cd3a9461ac0
SaefullohAziz <16171234@smkifsmd.sch.id>
2019-10-12 11:14:13 +0700

    finish teacher

--- resources/views/teacher/create.blade.php
+++ resources/views/teacher/create.blade.php
@@ -34,11 +34,18 @@
 						<div class="col-sm-6">
 							<fieldset>
 								<legend></legend>
+								{{ Form::bsText(null, __('NIP'), 'nip', old('nip'), __('NIP')) }}
+
 								{{ Form::bsText(null, __('Full Name'), 'name', old('name'), __('Full Name'), ['required' => '']) }}
+
+								{{ Form::bsText(null, __('Date of Birth'), 'date_of_birth', '01-01-1990', __('Date Of Birth'), ['required' => '']) }}
 
 								{{ Form::bsSelect(null, __('Gender'), 'gender', ['Laki-Laki' => 'Laki-Laki', 'Perempuan' => 'Perempuan'], old('gender'), __('Select'), ['placeholder' => __('Select'), 'required' => '']) }}
 
 								{{ Form::bsText(null, __('Position'), 'position', old('position'), __('Position'), ['required' => '']) }}
+
+								{{ Form::bsInlineRadio(null, __('Teaching Status?'), 'teaching_status', ['yes' => __('Yes'), 'No' => __('No')], old('teaching_status'), ['required' => '']) }}
+
 							</fieldset>
 						</div>
 						<div class="col-sm-6">
@@ -47,6 +54,8 @@
 								{{ Form::bsEmail(null, __('E-Mail'), 'email', old('email'), __('E-Mail'), ['required' => ''], [__('Required to use Gmail')]) }}
 
 								{{ Form::bsPhoneNumber(null, __('Phone Number'), 'phone_number', old('phone_number'), __('Phone Number'), ['maxlength' => '13', 'required' => '']) }}
+
+								{{ Form::bsTextarea(null, __('Address'), 'address', old('address'), __('Address')) }}
 
 								{{ Form::bsFile(null, __('Photo'), 'photo', old('photo'), [], [__('Photo with JPG/PNG format up to 5MB.')]) }}
 							</fieldset>
