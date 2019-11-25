8771d6c8d313e5a65d93812c76024b73507bf036
Ahmad Husen <whoishusen@gmail.com>
2019-11-25 15:48:31 +0700

    Add company model

---
+++ app/Company.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Traits\Uuids;
+
+class Company extends Model
+{
+    use Uuids, SoftDeletes;
+}
