6b87d31663748c13731a576d42108a7fd84f0026
saefullohaziz <16171234@smkifsmd.sch.id>
2019-12-09 17:03:41 +0700

    active multiple table reset password

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+use Illuminate\Http\Request;
+use App\User;
+use App\Admin\User as Admin;
+use Illuminate\Support\Facades\Password;
 
 class ForgotPasswordController extends Controller
 {
@@ -28,6 +32,43 @@ class ForgotPasswordController extends Controller
     public function __construct()
     {
         $this->middleware('guest');
+        $this->middleware('guest:admin');
+    }
+    
+    public function sendResetLinkEmail(Request $request)
+    {
+        $this->validateEmail($request);
+
+        // We will send the password reset link to this user. Once we have attempted
+        // to send the link, we will examine the response then see the message we
+        // need to show to the user. Finally, we'll send out a proper response.
+
+        $user = User::where('email', $request->email)->where('deleted_at', NULL)->count();
+        $admin = Admin::where('email',$request->email )->where('deleted_at', NULL)->count();
+
+        if ($user > 0) {
+            $response = $this->broker()->sendResetLink(
+                $this->credentials($request)
+            );
+        }
+        if ($admin > 0) {
+            $response = $this->brokerAdmin()->sendResetLink(
+                $this->credentials($request)
+            );
+        }
+        return $response == Password::RESET_LINK_SENT
+                    ? $this->sendResetLinkResponse($request, $response)
+                    : $this->sendResetLinkFailedResponse($request, $response);
+    }
+
+    public function broker()
+    {
+        return Password::broker(); 
+    }
+
+    public function brokerAdmin()
+    {
+        return Password::broker('admins'); 
     }
 
     /**
--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -4,6 +4,14 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use App\User;
+use App\Admin\User as Admin;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Auth\Events\PasswordReset;
+use Illuminate\Support\Str;
 
 class ResetPasswordController extends Controller
 {
@@ -35,6 +43,122 @@ class ResetPasswordController extends Controller
     public function __construct()
     {
         $this->middleware('guest');
+        $this->middleware('guest:admin');
+    }
+
+    /**
+     * Reset the given user's password.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
+     */
+    public function reset(Request $request)
+    {
+        $request->validate($this->rules(), $this->validationErrorMessages());
+
+        // Here we will attempt to reset the user's password. If it is successful we
+        // will update the password on an actual user model and persist it to the
+        // database. Otherwise we will parse the error and return the response.
+
+        $user = User::where('email', $request->email)->where('deleted_at', NULL)->count();
+        $admin = Admin::where('email',$request->email )->where('deleted_at', NULL)->count();
+
+        if ($user > 0) {
+            $response = $this->broker()->reset(
+                $this->credentials($request), function ($user, $password) {
+                    $this->resetPassword($user, $password);
+                }
+            );
+        }
+
+        if ($admin > 0) {
+            $response = $this->brokerAdmin()->reset(
+                $this->credentials($request), function ($admin, $password) {
+                    $this->resetPasswordAdmin($admin, $password);
+                }
+            );
+        }
+
+        // If the password was successfully reset, we will redirect the user back to
+        // the application's home authenticated view. If there is an error we can
+        // redirect them back to where they came from with their error message.
+        return $response == Password::PASSWORD_RESET
+                    ? $this->sendResetResponse($request, $response)
+                    : $this->sendResetFailedResponse($request, $response);
+    }
+
+     /**
+     * Reset the given user's password.
+     *
+     * @param  \Illuminate\Contracts\Auth\CanResetPassword  $user
+     * @param  string  $password
+     * @return void
+     */
+    protected function resetPassword($user, $password)
+    {
+        $user->password = Hash::make($password);
+
+        $user->setRememberToken(Str::random(60));
+
+        $user->save();
+
+        event(new PasswordReset($user));
+
+        $this->guard()->login($user);
+    }
+
+    /**
+     * Reset the given user's password.
+     *
+     * @param  \Illuminate\Contracts\Auth\CanResetPassword  $admin
+     * @param  string  $password
+     * @return void
+     */
+    protected function resetPasswordAdmin($admin, $password)
+    {
+        $admin->password = Hash::make($password);
+
+        $admin->setRememberToken(Str::random(60));
+
+        $admin->save();
+
+        event(new PasswordReset($admin));
+
+        $this->guardAdmin()->login($admin);
+    }
+
+    /**
+     * Get the password reset validation rules.
+     *
+     * @return array
+     */
+    protected function rules()
+    {
+        return [
+            'token' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|confirmed|min:8',
+        ];
+    }
+
+    /**
+     * Get the guard to be used during password reset.
+     *
+     * @return \Illuminate\Contracts\Auth\StatefulGuard
+     */
+    protected function guard()
+    {
+        return Auth::guard();
+    }
+
+    /**
+     * Get the guard to be used during password reset.
+     *
+     * @return \Illuminate\Contracts\Auth\StatefulGuard
+     */
+    protected function guardAdmin()
+    {
+        return Auth::guard('admin');
     }
 
     /**
@@ -47,5 +171,15 @@ class ResetPasswordController extends Controller
     public function showResetForm($token = null)
     {
         return view('auth.passwords.reset', ['title' => 'Reset Password', 'token' => $token]);
+    }
+
+    public function broker()
+    {
+        return Password::broker(); 
+    }
+
+    public function brokerAdmin()
+    {
+        return Password::broker('admins'); 
     }
 }
